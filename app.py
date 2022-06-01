4df49d1e519c51da0fd2154de656fcf6cdb6477d
Samuel Emmanuel <58962390+Sam-Emma@users.noreply.github.com>
2022-06-01 06:14:31 -1200

    Add files via upload

--- app.py
+++ app.py
@@ -9,20 +9,35 @@ def Home():
     FPG = ""
     age = ""
     bmi = ""
+    w_pregnancy = ""
+    sex =""
+    q_pregnancy =""
+    height = ""
+    weight = ""
     miscarriage = ""
     n_pregnancy = ""
     mess = ""
     if request.method == "POST":
         age = request.form["age"]
-        bmi = request.form["bmi"]
+        sex = request.form["sex"]
+        q_pregnancy = request.form["pregnant_q"]
+        height = request.form["height"]
+        weight = request.form["weight"]
         miscarriage = request.form["miscarriage"]
         n_pregnancy = request.form["n_pregnancy"]
-        predict_real, FPG, mess = model.predict(age,bmi,miscarriage,n_pregnancy)
+        w_pregnancy = request.form["w_pregnancy"]
+        predict_real, FPG, bmi, mess = model.predict(age,sex,q_pregnancy,
+                                    height, weight,
+                                    miscarriage,
+                                    n_pregnancy,w_pregnancy)
     return render_template("index.html",outcome = predict_real, 
                             fpg = FPG, 
                             age_input =age,bmi_input = bmi, 
                             miscarriage_input = miscarriage,
                             n_preg_input = n_pregnancy,
+                            w_preg_input = w_pregnancy,
+                            height_input = height,
+                            weight_input = weight,
                             message = mess
                     )
         
--- model.py
+++ model.py
@@ -6,54 +6,76 @@ def load_model():
     model = pickle.load(open("model.sav", 'rb'))
     return model
 
-def validation(age,bmi,miscarriage,n_pregnancy):
-    empty_spaces = 0
+def validation(age,sex,weight,height,
+              q_pregnancy,
+              miscarriage,n_pregnancy,w_pregnancy):
     error = 0
-    if(age == "" or bmi == "" or miscarriage == "" or n_pregnancy == "" ):
-        empty_spaces = empty_spaces + 1
-    elif int(age) < 13 or int(age) > 70:
-        error = error + 1
-    elif float(bmi) < 12.0 or float(bmi) > 80.0:
-        error = error + 1
+    message = ""
+    if(age == "" or  miscarriage == "" or
+       n_pregnancy == "" or height == "" or weight == "" ):
+        error = 1
+        message = "Fill in all the required data"
+    elif sex == "male" or sex == "null":
+        error = 1
+        message = "Only a female can take this test"
+    elif q_pregnancy == "no" or q_pregnancy == "null":
+        error = 1
+        message = "Only pregnant women can take this test"
+    elif w_pregnancy == "null":
+        error = 1
+        message = "Failed to enter the duration of pregnancy"
+    elif int(age) < 15 or int(age) > 51:
+        error = 1
+        message = "Allowed age range is 15 to 51"
+    elif float(height) < 0 or float(height) > 5:
+        error = 1
+        message = "Exceeded acceptable height"
+    elif float(weight) < 20 or float(weight) > 100:
+        error = 1
+        message = "Exceeded acceptable height"
     elif int(miscarriage) < 0 or int(miscarriage) > 6:
-        error = error + 1
+        error = 1
+        message = "Invalid input"
     elif int(n_pregnancy) < 0 or int(n_pregnancy) > 20:
-        error = error + 1
+        error = 1
+        message = "Invalid input"
     
-    return error, empty_spaces
+    return error, message, height, weight
     
     
 
-def predict(age,bmi,miscarriage,n_pregnancy):
-    error, empty_spaces = validation(age,bmi,miscarriage,n_pregnancy)
-    print(error," " ,empty_spaces)
+def predict(age,sex,q_pregnancy,
+                            height, weight,
+                            miscarriage,n_pregnancy,w_pregnancy):
+    
+    error, message, height, weight = validation(age,sex,
+                                weight,height, q_pregnancy,
+                                miscarriage,n_pregnancy, w_pregnancy)
+    
+    
     predict_real  = ""
     FPG = ""
-    message = ""
-    if empty_spaces > 0:
-        predict_real = ''
-        FPG = ''
-        message = "empty"
-    elif error > 0:
-        predict_real = ''
-        FPG = ''
-        message = "invalid"
+    bmi =""
+    if error != 0:
+        predict_real = '-'
+        FPG = '-'
     else:
+        bmi = float(weight)/(float(height)**2)
         X_dataframe = pd.DataFrame([{'AGE':int(age), 'BMI':float(bmi), 'Miscariage Before 6 month':int(miscarriage),
                                       'Number of Pregnancy':int(n_pregnancy)}])        
         predict_real, FPG = makePrediction(X_dataframe)
         FPG = np.round(FPG[0],2)
         message = 'success'
-    return predict_real, FPG, message
+    return predict_real, FPG, bmi, message
 
 def makePrediction(test_pre):
     classifier = load_model()
     predict_real = ""
     FPG = classifier.predict(test_pre)
     if FPG >= 5.10:
-        predict_real = "Positive"
+        predict_real = "At risk"
     else:
-        predict_real = "Negative"
+        predict_real = "Not at risk"
         
     return predict_real, FPG
 
